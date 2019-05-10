66d36a5d807ee76fde62c05fd2f21b23e0352170
Christopher Warner <christopher.warner92@gmail.com>
2019-05-10 18:57:49 -0400

    converted function to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Take out the trash',
+        completed: false
+      },
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+      <Todos />
+        </div>
+    );
+  }
 }
 
 export default App;
