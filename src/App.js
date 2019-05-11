00a6b197091d1f4b0ada5e9b176d219d9e856afd
Christopher Warner <christopher.warner92@gmail.com>
2019-05-10 20:18:35 -0400

    added some styling

--- src/App.js
+++ src/App.js
@@ -49,10 +49,12 @@ return todo;
     console.log(this.state.todos);
     return (
       <div className="App">
+      <div className="container">
       <Header />
       <AddTodo />
       <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
         </div>
+      </div>
     );
   }
 }
